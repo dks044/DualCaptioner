bd960d3ff337c51751103c116f9395c3862b0abd
unknown <buzz781@gmail.com>
2023-10-21 17:04:13 +0900

    한글캡션번역기능완성

--- src/main/ActorCaptionGenerator.java
+++ src/main/ActorCaptionGenerator.java
@@ -2,10 +2,12 @@ package main;
 import java.util.*;
 
 import Actor.Actor;
+import translator.Translator;
 
 public class ActorCaptionGenerator {
 
 	public static void main(String[] args) {
+		Translator t = new Translator();
 		
 		//==========엑터 객체 데이터 생성(시작)=====================================================
 		ArrayList<Actor> actor_list = new ArrayList<>();
@@ -26,7 +28,11 @@ public class ActorCaptionGenerator {
 				+ "0004	P01\r\n"
 				+ "0004	P02\r\n"
 				+ "0004	P03\r\n"
-				+ "0004	P04";
+				+ "0004	P04\r\n"
+				+ "0005	P01\r\n"
+				+ "0005	P02\r\n"
+				+ "0005	P03\r\n"
+				+ "0005	P04";
 		
 		String[] data_input_arr = data_input.split("\r\n");
 		
@@ -47,22 +53,74 @@ public class ActorCaptionGenerator {
 		String data_input2="0001	10대 미만	 남자	 셔츠	 면바지\r\n"
 				+ "0002	10대 미만	 남자	 셔츠	 면바지\r\n"
 				+ "0003	10대	 남자	 맨투맨	 운동복 바지/산악바지\r\n"
-				+ "0004	10대	 남자	 맨투맨	 운동복 바지/산악바지";
+				+ "0004	10대	 남자	 맨투맨	 운동복 바지/산악바지\r\n"
+				+ "0005	10대	 남자	 니트	 청바지";
+		
 		data_input2 = data_input2.replaceAll("\\s+(미만)", "$1");  //'미만' 글자 앞에 공백 없애기
 		data_input2 = data_input2.replaceAll("\\s+(바지)", "$1");  //'바지' 글자 앞에 공백 없애기
+		
+		//TODO:: 테스트중 (셔츠 면바지, 셔츠면바지 구분관련) 
+		data_input2 = data_input2.replaceAll("(?<=\\p{IsHangul})\\s+(면)", "$1");
+		data_input2 = data_input2.replaceAll("셔츠면", "셔츠 면");
+
+		
+		data_input2 = data_input2.replaceAll("\\s+(티)", "$1");  //'티' 글자 앞에 공백 없애기
+		data_input2 = data_input2.replaceAll("\\s+(카)", "$1");  //'카' 글자 앞에 공백 없애기
+		
+		// '셔츠'와 '면바지' 사이의 공백을 탭으로 변환
+		data_input2 = data_input2.replaceAll("(셔츠)\\s+(면바지)", "$1\t$2");
+
 		
 		String[] data_input_arr2 = data_input2.split("\r\n");
 		
 		for(int i=0;i<data_input_arr2.length;i++) {
+			int index =0;
 			String[] data_input_temp2 = data_input_arr2[i].split("\t"); // 탭 문자로 분리
+			
 			Actor tempActor2 = Actor.findActorByNo(data_input_temp2[0]);
 			tempActor2.setAge(data_input_temp2[1]);
 			tempActor2.setGender(data_input_temp2[2]);
+			
+			if(data_input_temp2[3].equals("반팔면티셔츠")) data_input_temp2[3] = "반팔 면 티셔츠";
+			if(data_input_temp2[3].equals("반팔카라티셔츠")) data_input_temp2[3] = "반팔 카라티셔츠";
 			tempActor2.setTop(data_input_temp2[3]);
+			
+			if(data_input_temp2[4].equals("나일론바지")) data_input_temp2[4] = "나일론 바지";
+			if(data_input_temp2[4].equals("운동복바지/산악바지")) data_input_temp2[4] = "운동복/산악바지";
 			tempActor2.setBottom(data_input_temp2[4]);
 		}
 		//==========엑터 객체 데이터 생성(끝)=====================================================
+		StringBuilder k = new StringBuilder();
+		StringBuilder e = new StringBuilder();
 		
+		
+		//한글캡션
+		for(Actor a : actor_list) {
+			for(int i=0;i<a.getAct_pose().size();i++) {
+				for(int j=0;j<t.getCamKr().length;j++) {
+					//k.append(a.getActor_no()+" ");//테스트
+					k.append("흰색 배경에서 ");
+					if(a.getAge().contains("미만"))k.append("10대 미만");
+					else k.append(a.getAge()+" ");
+					k.append(a.getGender()+"가 ");
+					k.append(a.getTop()+"와 ");
+					k.append(a.getBottom()+"를 입고 ");
+					k.append(t.getPose_kr_map().get(a.getAct_pose().get(i))+"를 취하고 있는 ");
+					String[] camKR = t.getCamKr();
+					k.append(camKR[j]);
+					System.out.println(k.toString());
+					k.setLength(0);
+				}
+			}
+		}
+		System.out.println("==========================================================================================");
+		System.out.println("==========================================================================================");
+		System.out.println("==========================================================================================");
+		System.out.println("==========================================================================================");
+		//영어캡션
+		for(Actor a : actor_list) {
+			
+		}
 		
 		
 		
--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -1,5 +1,148 @@
 package translator;
 
-public class Translator {
+import java.util.HashMap;
+import java.util.*;
 
+public class Translator {
+	//캠
+	static final String[] CAM_KR = {"정면 모습","좌측 정면 모습","우측 후면 모습","후면 모습","좌측 후면 모습","우측 정면 모습"};
+	static final String[] CAM_ENG = {"on front view","on left front view","on right back view","on back view",
+			"on left back view","on right front view"};
+	//TODO:: 성별은 알아서 코드안에
+	
+	
+	// 포즈
+	static HashMap<String,String> pose_kr_map = new HashMap<String, String>();
+	static HashMap<String,String> pose_eng_map = new HashMap<String, String>();
+
+	//의상
+    static HashMap<String, String> clothes_map = new HashMap<>();
+    //연령대
+    static HashMap<String, String> age_map = new HashMap<>();
+    
+    
+    
+    
+    static {  // static 초기화 블록 시작
+    	 pose_kr_map.put("P01", "바로 선 자세");
+         pose_kr_map.put("P02", "머리 위로 팔 뻗은 자세");
+         pose_kr_map.put("P03", "주먹 맞댄 선 자세");
+         pose_kr_map.put("P04", "팔 굽힌 선 자세");
+         pose_kr_map.put("P05", "아래 팔 올린 선 자세");
+         pose_kr_map.put("P06", "양팔 뻗은 선 자세");
+         pose_kr_map.put("P07", "바로 앉은 자세");
+         pose_kr_map.put("P08", "응용 앉은 자세");
+         pose_kr_map.put("P09", "양팔 T자 선 자세");
+         pose_kr_map.put("P10", "양팔 A자 선 자세");
+         
+         pose_eng_map.put("P01", "Standing_straight");
+         pose_eng_map.put("P02", "inverting_both_arms_at_90_degree_at_shoulder_level");
+         pose_eng_map.put("P03", "facing_both_fists_together");
+         pose_eng_map.put("P04", "folding_right_arm");
+         pose_eng_map.put("P05", "raising_up_right_forearm");
+         pose_eng_map.put("P06", "stratching_both_arms_forward");
+         pose_eng_map.put("P07", "sitting_straight");
+         pose_eng_map.put("P08", "sitting_applied");
+         pose_eng_map.put("P09", "posing_T_posture_with_both_arms");
+         pose_eng_map.put("P10", "posing_A_posture_with_both_arms");
+    	
+    	
+        clothes_map.put("맨투맨", "sweatshirts");
+        clothes_map.put("셔츠", "shirts");
+        clothes_map.put("니트", "knit");
+        clothes_map.put("반팔 면 티셔츠", "short-sleeve T-shirt");
+        clothes_map.put("반팔 카라티셔츠", "collar-tshirt");
+        clothes_map.put("반팔셔츠", "short-sleeve-shirt");
+        clothes_map.put("면바지", "cotton-pants");
+        clothes_map.put("청바지", "jeans");
+        clothes_map.put("운동복/산악바지", "hiking-pants");
+        clothes_map.put("면 반바지", "cotton-shorts");
+        clothes_map.put("나일론 바지", "nylon-trousers");
+        clothes_map.put("통바지", "wide-pants");
+        clothes_map.put("치마", "skirt");
+        
+        age_map.put("10대 미만", "An under 10s");
+        age_map.put("10대", "An teenager");
+        age_map.put("20대", "An 20s");
+        age_map.put("30대", "An 30s");
+        age_map.put("40대", "An 40s");
+        age_map.put("50대", "An 50s");
+        age_map.put("60대", "An 60s");
+        age_map.put("70대", "An 70s");
+        age_map.put("80대", "An 80s");
+        
+        
+    }  
+	
+
+
+
+
+	public static HashMap<String, String> getPose_kr_map() {
+		return pose_kr_map;
+	}
+
+
+
+
+	public static void setPose_kr_map(HashMap<String, String> pose_kr_map) {
+		Translator.pose_kr_map = pose_kr_map;
+	}
+
+
+
+
+	public static HashMap<String, String> getPose_eng_map() {
+		return pose_eng_map;
+	}
+
+
+
+
+	public static void setPose_eng_map(HashMap<String, String> pose_eng_map) {
+		Translator.pose_eng_map = pose_eng_map;
+	}
+
+
+
+
+	public static HashMap<String, String> getClothes_map() {
+		return clothes_map;
+	}
+
+
+
+
+	public static void setClothes_map(HashMap<String, String> clothes_map) {
+		Translator.clothes_map = clothes_map;
+	}
+
+
+
+
+	public static HashMap<String, String> getAge_map() {
+		return age_map;
+	}
+
+
+
+
+	public static void setAge_map(HashMap<String, String> age_map) {
+		Translator.age_map = age_map;
+	}
+
+
+
+
+	public static String[] getCamKr() {
+		return CAM_KR;
+	}
+
+
+
+
+	public static String[] getCamEng() {
+		return CAM_ENG;
+	}
+	
 }
